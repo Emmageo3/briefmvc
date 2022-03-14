09d9eb55b4b86cbc8e3497a1c30e8990322e6e74
emmageo3 <kanfanyemma22@gmail.com>
2022-03-14 09:54:38 +0000

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e4e965ec8d81a2d6c08eadb453785ffb4b2442b1',
+        'name' => 'emma/brief_mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emma/brief_mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e4e965ec8d81a2d6c08eadb453785ffb4b2442b1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
